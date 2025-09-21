6e29c04253ab67a820142679aff458ccbd329a13
huzejun <961639130@qq.com>
2025-09-21 17:33:14 +0800

    父项目聚合与mapper4一键生成dao 2025-9-21 17:33:01

---
+++ mybatis_generator2025/src/main/java/com/dream/cloud/Main.java
@@ -0,0 +1,11 @@
+package com.dream.cloud;
+
+/**
+ * @Author huzejun
+ * @Date 2025-09-21 16:52
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ mybatis_generator2025/src/main/java/com/dream/cloud/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package com.dream.cloud.mapper;
+
+import com.dream.cloud.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
