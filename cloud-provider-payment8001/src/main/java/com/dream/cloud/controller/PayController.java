81fafa26fc19d831f5cae5d98e5ece49cf4c2e54
huzejun <961639130@qq.com>
2025-09-22 19:44:36 +0800

    pay8001项目实战-swagger3的使用 2025-9-22 19:44:23

--- cloud-provider-payment8001/src/main/java/com/dream/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/dream/cloud/controller/PayController.java
@@ -4,6 +4,8 @@ import com.dream.cloud.entities.Pay;
 import com.dream.cloud.entities.PayDTO;
 import com.dream.cloud.resp.ResultData;
 import com.dream.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -17,12 +19,14 @@ import java.util.List;
  **/
 @RestController
 @Slf4j
+@Tag(name = "支付微服务模块",description = "支付CRUD")
 public class PayController {
     
     @Resource
     private PayService payService;
     
     @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
     public String addPay(@RequestBody Pay pay) {
         System.out.println(pay.toString());
         int i = payService.add(pay);
@@ -30,11 +34,13 @@ public class PayController {
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "删除",description = "删除支付流水方法")
     public Integer deletePay(@PathVariable("id") Integer id) {
         return payService.delete(id);
     }
 
     @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改",description = "修改支付流水方法")
     public String updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
 
@@ -46,6 +52,7 @@ public class PayController {
     }
 
     @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "按照ID流水",description = "查询支付流水方法")
     public Pay getById(@PathVariable("id") Integer id) {
         return payService.getById(id);
     }
--- cloud-provider-payment8001/src/main/java/com/dream/cloud/entities/Pay.java
+++ cloud-provider-payment8001/src/main/java/com/dream/cloud/entities/Pay.java
@@ -1,5 +1,7 @@
 package com.dream.cloud.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.*;
@@ -8,32 +10,38 @@ import javax.persistence.*;
  * 表名：t_pay
 */
 @Table(name = "t_pay")
+@Schema(title = "支付交易表实体类")
 public class Pay {
     @Id
     @GeneratedValue(generator = "JDBC")
+    @Schema(title = "主键")
     private Integer id;
 
     /**
      * 支付流水号
      */
     @Column(name = "pay_no")
+    @Schema(title = "支付流水号")
     private String payNo;
 
     /**
      * 订单流水号
      */
     @Column(name = "order_no")
+    @Schema(title = "订单流水号")
     private String orderNo;
 
     /**
      * 用户账号ID
      */
     @Column(name = "user_id")
+    @Schema(title = "用户账号ID")
     private Integer userId;
 
     /**
      * 交易金额
      */
+    @Schema(title = "交易金额")
     private BigDecimal amount;
 
     /**
@@ -45,12 +53,14 @@ public class Pay {
      * 创建时间
      */
     @Column(name = "create_time")
+    @Schema(title = "创建时间")
     private Date createTime;
 
     /**
      * 更新时间
      */
     @Column(name = "update_time")
+    @Schema(title = "更新时间")
     private Date updateTime;
 
     /**
