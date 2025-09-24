45393b675f1f924133cf0908d9a0c7c020909911
huzejun <961639130@qq.com>
2025-09-24 20:47:34 +0800

    cloud-api-commons公共代码模块 2025-9-24 20:47:24

---
+++ cloud-api-commons/src/main/java/com/dream/cloud/Main.java
@@ -0,0 +1,11 @@
+package com.dream.cloud;
+
+/**
+ * @Author huzejun
+ * @Date 2025-09-24 18:05
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- cloud-api-commons/src/main/java/com/dream/cloud/entities/PayDTO.java
+++ cloud-api-commons/src/main/java/com/dream/cloud/entities/PayDTO.java
@@ -9,20 +9,25 @@ import java.math.BigDecimal;
 
 /**
  * @Author huzejun
- * @Date 2025-09-21 18:38
+ * @Date 2025-09-24 16:23
+ * 一般而言，调用者不应该熟悉服务提供者的entity资源并知道表结构关系，所以服务提供方给出的
+ *      接口文档都应成为DTO
  **/
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class PayDTO implements Serializable {
 
     private Integer id;
+
     //支付流水号
     private String payNo;
     //订单流水号
     private String orderNo;
+
     //用户账号ID
     private Integer userId;
+
     //交易金额
     private BigDecimal amount;
 }
