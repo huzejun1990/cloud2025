5456259fbc038caca214b87a80d4dc5f2343ceef
huzejun <961639130@qq.com>
2025-10-11 15:58:19 +0800

    gateway之Predication配置使用 path、after、Before、Cookie、Header、Host、Query、RemoteAddr等 2025-10-11 15:58:04

--- cloud-api-commons/src/main/java/com/dream/cloud/Main.java
+++ cloud-api-commons/src/main/java/com/dream/cloud/Main.java
@@ -1,12 +1,18 @@
 package com.dream.cloud;
 
+import java.time.ZonedDateTime;
+
 /**
  * @Author huzejun
  * @Date 2025-09-24 18:05
  **/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+
+        ZonedDateTime zbj = ZonedDateTime.now();  //默认时区
+        System.out.println(zbj);
+
     }
 
 }
