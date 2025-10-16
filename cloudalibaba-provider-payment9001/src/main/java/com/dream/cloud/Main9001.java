e5e03af1fd03c0b8b6d90be127d2ed67ad886ecf
huzejun <961639130@qq.com>
2025-10-16 14:06:35 +0800

    nacos服务提供者9001入驻nacos中心 2025-10-16 14:06:15

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/dream/cloud/Main9001.java
@@ -0,0 +1,17 @@
+package com.dream.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author huzejun
+ * @Date 2025-10-14 18:16
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/dream/cloud/controller/PayAlibabaController.java
@@ -0,0 +1,22 @@
+package com.dream.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author huzejun
+ * @Date 2025-10-14 18:24
+ **/
+@RestController
+public class PayAlibabaController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id) {
+        return "nacos registry,serverPort: " + serverPort+"\t id"+id;
+    }
+}
