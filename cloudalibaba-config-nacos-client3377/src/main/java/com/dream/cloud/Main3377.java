d563e85ecaa1ef9cf0a370fb1501a0af481cfd32
huzejun <961639130@qq.com>
2025-10-16 23:46:00 +0800

    Nacos与Namespace-Group-Datald三元组操作 2025-10-16 23:45:42

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/dream/cloud/Main3377.java
@@ -0,0 +1,17 @@
+package com.dream.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author huzejun
+ * @Date 2025-10-16 15:45
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Main3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main3377.class, args);
+    }
+}
