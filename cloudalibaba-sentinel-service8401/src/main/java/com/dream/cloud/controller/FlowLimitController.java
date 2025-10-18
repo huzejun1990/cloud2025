3284d42a8f3a094ddcf1719ba20abd0c0c87b6b9
huzejun <961639130@qq.com>
2025-10-18 17:04:48 +0800

    Sentinel之流控模式（预热WarmUp、排队等待、并发线程数）2025-10-18 17:04:34

--- cloudalibaba-sentinel-service8401/src/main/java/com/dream/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dream/cloud/controller/FlowLimitController.java
@@ -5,6 +5,8 @@ import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author huzejun
  * @Date 2025-10-17 14:41
@@ -52,5 +54,33 @@ public class FlowLimitController {
         flowLimitService.common();
         return "---------------testD";
     }
+
+    /**
+     * 流控效果----排队等待
+     * @return
+     */
+    @GetMapping("/testE")
+    public String testE() {
+        System.out.println(System.currentTimeMillis() + "      testE,流控效果----排队等待");
+        return "---------------testE";
+    }
+
+    /**
+     * 新增熔断规则-慢调用比例
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF() {
+        //暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(System.currentTimeMillis() + "----测试：新增熔断规则-慢调用比例 ");
+        return "---------------testF 新增熔断规则-慢调用比例 ";
+    }
+
+
 
 }
