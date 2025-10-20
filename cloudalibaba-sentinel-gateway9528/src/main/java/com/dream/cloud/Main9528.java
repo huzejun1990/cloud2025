8c749040d8bcba74a1f156aac4fcb87f61ee87f0
huzejun <961639130@qq.com>
2025-10-20 13:26:31 +0800

    Sentinel之GateWay和Sentinel集成实现服务限流 2025-10-20 13:24:28

---
+++ cloudalibaba-sentinel-gateway9528/src/main/java/com/dream/cloud/Main9528.java
@@ -0,0 +1,17 @@
+package com.dream.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author huzejun
+ * @Date 2025-10-19 18:49
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main9528 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main9528.class, args);
+    }
+}
